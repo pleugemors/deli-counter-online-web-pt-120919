dcdaa69ee14e2a3eb0b99a9de268c3548ef02dd1
Paul Leugemors <pleugemors@yahoo.com>
2019-12-09 13:32:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(customers)
     text = "The line is currently: "
     customers.each.with_index do |name, index|
       text += " "
-      text +=  ". " + name 
+      text +=  index + ". " + name 
     end
     puts text
   end
