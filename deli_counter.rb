522b2c5de02bb19b32ae3cd128ecf9ca248efeab
Paul Leugemors <pleugemors@yahoo.com>
2019-12-09 13:34:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(customers)
   if customers.size == 0 
     puts "The line is currently empty."
   else
-    text = "The line is currently: "
+    text = "The line is currently:"
     customers.each.with_index do |name, index|
       text += " "
       text +=  (index + 1).to_s  + ". " + name 
